744b106130e67bffd1beaca95c8d929cfeb94f12
MidhunReddyS <sannareddymidhunreddy@gmail.com>
2025-06-18 01:09:37 +0530

    git hub deploy

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,4 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Protected from "./protected";
 import Home from "../Pages/home";
 import Signin from "../Pages/signin";
@@ -6,26 +6,26 @@ import Signup from "../Pages/signup";
 import Verify from "../pages/verify";
 import ErrorPage from "../pages/error-page";
 import Public from "./public";
-  
-const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/">
-        <Route element={<Protected />}>
-          <Route index element={<Home />}/>
-        </Route>
-        <Route element={<Public />}>
-          <Route path="signin" element={<Signin />} />
-          <Route path="verify" element={<Verify />} />
-          <Route path="signup" element={<Signup />} />
-        </Route>
-        <Route path="error-page" element={<ErrorPage />} />
-        <Route path="*" element={<h1>Page not found</h1>} />
-      </Route>
-    )
-);
 
 const Index = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter basename="/nxtwave-frontend/">
+      <Routes>
+        <Route path="/">
+          <Route element={<Protected />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route element={<Public />}>
+            <Route path="signin" element={<Signin />} />
+            <Route path="verify" element={<Verify />} />
+            <Route path="signup" element={<Signup />} />
+          </Route>
+          <Route path="error-page" element={<ErrorPage />} />
+          <Route path="*" element={<h1>Page not found</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Index;
