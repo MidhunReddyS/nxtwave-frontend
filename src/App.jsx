a07ffb238a088b063b24a225016ba0195c3c5ff3
MidhunReddyS <sannareddymidhunreddy@gmail.com>
2025-06-18 01:24:41 +0530

    git hub deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import Index from './routes';
 
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-sky-500 to-indigo-500">
       <Index />
-    </div>
   )
 }
